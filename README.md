0ddbe81196f153c40c03302a80040187769a05f9
Patrick Waweru <patrick.waweru@thepalladiumgroup.com>
2022-02-21 11:29:55 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+EVMMC OpenMRS Module
+========================
+<a href="https://ci.kenyaemr.org/viewType.html?buildTypeId=evmmc_mvn"><img src="https://ci.kenyaemr.org/app/rest/builds/buildType:evmmc_mvn/statusIcon"/></a>
+
+Overview
+--------
+This module provides EVMMC functionality. It is part of the [KenyaEMR distribution](https://github.com/I-TECH/openmrs-distro-kenyaemr).
---
+++ api/src/main/java/org/openmrs/module/evmmc/api/impl/EvmmcServiceImpl.java
@@ -0,0 +1,52 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.evmmc.api.impl;
+
+import org.openmrs.api.APIException;
+import org.openmrs.api.UserService;
+import org.openmrs.api.impl.BaseOpenmrsService;
+import org.openmrs.module.evmmc.Item;
+import org.openmrs.module.evmmc.api.EvmmcService;
+import org.openmrs.module.evmmc.api.dao.EvmmcDao;
+
+public class EvmmcServiceImpl extends BaseOpenmrsService implements EvmmcService {
+	
+	EvmmcDao dao;
+	
+	UserService userService;
+	
+	/**
+	 * Injected in moduleApplicationContext.xml
+	 */
+	public void setDao(EvmmcDao dao) {
+		this.dao = dao;
+	}
+	
+	/**
+	 * Injected in moduleApplicationContext.xml
+	 */
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@Override
+	public Item getItemByUuid(String uuid) throws APIException {
+		return dao.getItemByUuid(uuid);
+	}
+	
+	@Override
+	public Item saveItem(Item item) throws APIException {
+		if (item.getOwner() == null) {
+			item.setOwner(userService.getUser(1));
+		}
+		
+		return dao.saveItem(item);
+	}
+}
